9a77159a25d186f9d9a865c1fee7e7bd11bd7d65
Zhilskiy, Dmitriy <Dmitriy.Zhilskiy@ocsico.com>
2018-12-19 11:37:40 +0300

    fix storage access issue

--- workspace/entrypoint.sh
+++ workspace/entrypoint.sh
@@ -3,6 +3,7 @@
 composer install && chmod -R 777 vendor
 php -r "file_exists('.env') || copy('.env.example', '.env');" && chmod -R 777 .env
 php artisan migrate --force --seed
+chmod -R 777 storage/
 
 tail -f /dev/null
 #exec "$@";
